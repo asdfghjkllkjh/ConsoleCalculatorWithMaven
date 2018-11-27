bbd1164f6473297c36e7d6216006c01ebd92f50d
Noah Stalder <noah.stalder@ergon.ch>
2018-11-27 12:39:03 +0100

    Addition und Substraction implementieren.

---
+++ src/main/java/ch/noah/Calculator.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 Ergon Informatik AG
+ * Merkurstrasse 43, 8032 Zuerich, Switzerland
+ * All rights reserved.
+ */
+
+package ch.noah;
+
+public class Calculator {
+
+    public int summe (int summand1, int summand2) {
+        return summand1 + summand2;
+    }
+}
