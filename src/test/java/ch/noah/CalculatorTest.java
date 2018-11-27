59e508b8f52a5f176b2ab27d3fee39a97262f2f0
Noah Stalder <noah.stalder@ergon.ch>
2018-11-27 15:17:45 +0100

    Test Calculator class with unittest.

---
+++ src/test/java/ch/noah/CalculatorTest.java
@@ -0,0 +1,24 @@
+package ch.noah;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest {
+    private static Calculator calculator;
+
+    @BeforeClass
+    public static void setCalculator () {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void shouldAddTwoNumbers() {
+        assertEquals(15, calculator.summe(5,10));
+    }
+}
