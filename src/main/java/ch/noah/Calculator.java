816ed30693110b3b40347d5a8f208e278ecbc653
Noah Stalder <noah.stalder@ergon.ch>
2018-12-04 12:12:17 +0100

    Implement division

--- src/main/java/ch/noah/Calculator.java
+++ src/main/java/ch/noah/Calculator.java
@@ -8,11 +8,15 @@ package ch.noah;
 
 public class Calculator {
 
-    public int summe (int summand1, int summand2) {
+    public int summe(int summand1, int summand2) {
         return summand1 + summand2;
     }
 
-    public int subtraktion (int value1, int value2) {
+    public int subtraktion(int value1, int value2) {
         return value1 - value2;
+    }
+
+    protected int division(int value1, int value2) {
+        return value1 / value2;
     }
 }
